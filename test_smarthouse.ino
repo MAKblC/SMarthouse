6b07b88d7cdb4a1fb68c40bf80532ab01db3a950
Max <41009925+MAKblC@users.noreply.github.com>
2023-12-12 14:56:19 +0300

    Update test_smarthouse.ino

--- test_smarthouse.ino
+++ test_smarthouse.ino
@@ -278,8 +278,12 @@ void setup()
 #endif
 
   bool bme_status = bme280.begin();
-  if (!bme_status)
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");  // проверка  датчика температуры, влажности и давления // checking the temp hum bar sensor
+  if (!bme_status) {
+    Serial.println("Не найден по адресу 0х77, пробую другой...");
+    bme_status = bme280.begin(0x76);
+    if (!bme_status)
+      Serial.println("Датчик не найден, проверьте соединение");
+  }
 
   ledcSetup(5, 50, 10);
   ledcAttachPin(amper, 5);
